dec86cf53117749cf7f5502a055c9ffd2334c8f6
godigitinsurance <59437973+godigitinsurance@users.noreply.github.com>
2020-07-31 22:08:06 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,13 @@
+$("#customerName").text("Meena Sharma");
+$("#policyNumber").text("EC72000874");
+$("#startDate").text("01-aUG-2020");
+$("#endDate").text("31-Jul-2021");
+
+
+$("#type").text("Four Wheeler");
+$("#registrationNumber").text("PB08Y5116");
+$("#make").text("HYUNDI ");
+$("#model").text("SANTRO ");
+$("#vehicalSubType").text("L2");
+$("#engineNo").text("0020622");
+$("#chassis").text("MAHAA51GRXM020731");
